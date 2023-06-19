cdaa6221eaebbfd3fc6d6b101f2f2c5af4fa5d0a
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-19 17:29:45 +0200

    ADD wile cycle in js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,19 @@ const shoppingList = [
     'milk'
 ]
 
-//FOR CYCLE
+/*FOR CYCLE
 for (let i = 0; i < shoppingList.length; i++) {
     let newLi = document.createElement('li')
     newLi.innerHTML = shoppingList[i];
     document.getElementById('list-items').append(newLi);
+}*/
+
+let i = 0;
+while (i < shoppingList.length) {
+
+    let newLi = document.createElement('li')
+    newLi.innerHTML = shoppingList[i];
+    document.getElementById('list-items').append(newLi);
+
+    i++
 }
